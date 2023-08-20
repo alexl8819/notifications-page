a59e7364c11ea0381d4d5e6b221d035ce2515ad1
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-08-20 21:02:32 +0000

    Deploying to gh-pages from @ alexl8819/notifications-page@2e47c2e873d383f5c60894db30a4cfc7b5e1928a 🚀

---
+++ index.31d422ee.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("theme-switch");e.checked="",e.addEventListener("change",()=>document.documentElement.classList.toggle("dark"))});
+//# sourceMappingURL=index.31d422ee.js.map
