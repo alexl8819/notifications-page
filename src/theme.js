2e47c2e873d383f5c60894db30a4cfc7b5e1928a
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-08-20 14:01:55 -0700

    refactor: update parcel, use solid icons only, remove unnecessary toggles in theme.js, add sr-only class for theme toggle label to fix accessibility issue

--- src/theme.js
+++ src/theme.js
@@ -1,10 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   const themeSwitch = document.getElementById('theme-switch');
   themeSwitch.checked = '';
-  themeSwitch.addEventListener('change', () => {
-    const themeIcon = document.querySelector('#theme-switch + label > i');
-    themeIcon.classList.toggle('fa-regular');
-    themeIcon.classList.toggle('fa-solid');
-    document.documentElement.classList.toggle('dark')
-  });
+  themeSwitch.addEventListener('change', () => document.documentElement.classList.toggle('dark'));
 });
